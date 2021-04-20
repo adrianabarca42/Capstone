6dbc81f24182cb9a105fd4c5f4e63664075d249c
Adrian Abarca <adrianabarca321@gmail.com>
2021-04-19 21:20:54 -0700

    Completed API endpoints POST /actors, POST /movies, PATCH /actors, PATCH /movies. Completed error handling for all potential errors.

--- app.py
+++ app.py
@@ -78,5 +78,118 @@ def delete_movies(payload, id):
   except:
     abort(422)
 
+@APP.route('/actors', methods=['POST'])
+@requires_auth('post:actors')
+def post_actors(payload):
+  if not request.method == 'POST':
+    abort(405)
+  body = request.get_json()
+  name = body.get('name')
+  age = body.get('age')
+  gender = body.get('gender')
+  new_actor = Actor(name=name, age=age, gender=gender)
+  new_actor.insert()
+  try:
+    return jsonify({
+      'success': True,
+      'actor': new_actor.name
+    }), 200
+  except:
+    abort(422)
+
+@APP.route('/movies', methods=['POST'])
+@requires_auth('post:movies')
+def post_actors(payload):
+  if not request.method == 'POST':
+    abort(405)
+  body = request.get_json()
+  title = body.get('title')
+  release_date = body.get('release_date')
+  actors = body.get('actors')
+  new_movie = Movie(title=title, release_date=release_date, actors=actors)
+  new_movie.insert()
+  try:
+    return jsonify({
+      'success': True,
+      'actor': new_movie.title
+    }), 200
+  except:
+    abort(422)
+
+@APP.route('/actors/<int:id>', methods=['PATCH'])
+@requires_auth('patch:actors')
+def patch_actors(payload, id):
+  if not request.method == 'PATCH':
+    abort(405)
+  actor = Actor.query.filter(Actor.id == id).one_or_none()
+  if actor is None:
+    abort(404)
+  body = request.get_json()
+  name = body.get('name')
+  age = body.get('age')
+  gender = body.get('gender')
+  actor.name = name
+  actor.age = age
+  actor.gender = gender
+  actor.update()
+  try:
+    return jsonify({
+      'success': True,
+      'actor': actor.name
+    }), 200
+  except:
+    abort(422)
+
+@APP.route('/movies/<int:id>', methods=['PATCH'])
+@requires_auth('patch:movies')
+def patch_movies(payload, id):
+  if not request.method == 'PATCH':
+    abort(405)
+  movie = Movie.query.filter(Movie.id == id).one_or_none()
+  if movie is None:
+    abort(404)
+  body = request.get_json()
+  title = body.get('title')
+  release_date = body.get('release_date')
+  actors = body.get('actors')
+  movie.title = title
+  movie.release_date = release_date
+  movie.actors = actors
+  movie.update()
+  try:
+    return jsonify({
+      'success': True,
+      'actor': movie.title
+    }), 200
+  except:
+    abort(422)
+
+## Error Handling
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+                "success": False, 
+                "error": 422,
+                "message": "unprocessable"
+                }), 422
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return jsonify({
+                "success": False, 
+                "error": 404,
+                "message": "resource not found"
+                }), 404
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+                "success": False, 
+                "error": 405,
+                "message": "method not allowed"
+                }), 405
+
+
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080, debug=True)

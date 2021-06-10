831f2a68e48f9e056afc78e943abf5878711b78b
Adrian Abarca <adrianabarca321@gmail.com>
2021-06-09 18:38:39 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+export AUTH0_DOMAIN='fsnd-practice1.us.auth0.com'
+export API_AUDIENCE='movie'
+export ALGORITHMS=['RS256']
+export DATABASE_URL='postgres://adrianabarca@localhost:5432/movie_test'
